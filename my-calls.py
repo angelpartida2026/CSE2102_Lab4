9fecfd3ad58c89313bd4793b43b752180763b567
angelpartida2026 <123526311+angelpartida2026@users.noreply.github.com>
2024-10-23 01:08:31 +0000

    Changes made to both files in order to complete the lab

--- my-calls.py
+++ my-calls.py
@@ -1,26 +1,32 @@
 import httpx
 
-url = "https://cautious-doodle-pjpjx694jxvcr944-5000.app.github.dev/"
+url = "https://automatic-disco-56pr9745w95c7pq6-5000.app.github.dev/"
 
 response = httpx.get(url)
 print(response.status_code)
 print(response)
-
-
 
 response = httpx.get(url)
 print(response.status_code)
 print(response.text)
 
 mydata = {
-    "text": "Hello Phil!",
+    "text": "Hello Angel!",
     "param2": "Making a POST request",
     "body": "my own value"
 }
 
 # A POST request to the API
 response = httpx.post(url + "echo", data=mydata)
+print(response.status_code)
+print(response.text)
+
+auth_data = {
+    "id": "angel.partida@uconn.edu",
+    "token": "f99aa8b8573062e9802f4fc0807ae1cb"
+}
 
 # Print the response
+response = httpx.post(url + "login", data=auth_data)
 print(response.status_code)
 print(response.text) 
--- my-server.py
+++ my-server.py
@@ -1,15 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+users = {
+    "angel.partida@uconn.edu": "f99aa8b8573062e9802f4fc0807ae1cb"
+}
+
 @app.route("/")
 def hello():
-   return " you called \n"
+   return "You called \n"
 
 # curl -d "text=Hello!&param2=value2" -X POST http://localhost:5000/echo
 @app.route("/echo", methods=['POST'])
 def echo():
    return "You said: " + request.form['text']
+
+@app.route("/login", methods=['POST'])
+def login():
+   auth_data = request.form
+   user_id = auth_data.get('id')
+   token = auth_data.get('token')
+   if users.get(user_id) == token:
+       return jsonify({"Login successful"}), 200
+   else:
+       return jsonify({"Invalid credentials"}), 401
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0')
